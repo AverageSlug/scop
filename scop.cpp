f71878668f23df7f18b8f72f4cd77a3de01d9eda
Nicolaj Lauridsen <nicolaj_lauridsen@hotmail.be>
2023-03-15 18:00:57 +0100

    udbgefjwbfuewinrbehef ajmbegrn

--- scop.cpp
+++ scop.cpp
@@ -10,28 +10,38 @@
 
 t_vertex	*vertices = NULL;
 t_face		*faces = NULL;
-float		q = -M_PI;
+float		q = M_PI;
 float		axis[7] = {0, 0, 0, 0, 0, 0, 0};
 int			n = 0;
+bool		tex_apply = false;
+int			width, height;
+const char	*data;
+GLuint texture;
 
 void	display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
 	t_face	*tmp = faces;
+	int		f = 0;
 	while (tmp)
 	{
 		glBegin(GL_POLYGON);
 			for (int i = 0; i < tmp->len; i++)
 			{
-				glColor3f(1, 1, 1); glVertex3f(((vertices[tmp->points[i] - 1].x - (axis[0] + axis[1]) / 2) * cos(q) - (vertices[tmp->points[i] - 1].z - (axis[4] + axis[5]) / 2) * sin(q) + (axis[0] + axis[1]) / 2) / (axis[6] * 1.2), (vertices[tmp->points[i] - 1].y) / (axis[6] * 1.2), ((vertices[tmp->points[i] - 1].z - (axis[4] + axis[5]) / 2) * cos(q) + (vertices[tmp->points[i] - 1].x - (axis[0] + axis[1]) / 2) * sin(q) + (axis[4] + axis[5]) / 2) / (axis[6] * 1.2));
+				if (tex_apply)
+					glTexCoord2f(((vertices[tmp->points[i] - 1].x - (axis[0] + axis[1]) / 2) * cos(q) - (vertices[tmp->points[i] - 1].z - (axis[4] + axis[5]) / 2) * sin(q) + (axis[0] + axis[1]) / 2) / (axis[6] * 1.2), (vertices[tmp->points[i] - 1].y) / (axis[6] * 1.2));
+				else
+					glColor3f((f % 3) == 0 ? 1 : 0, (f % 3) == 1 ? 1 : 0, (f % 3) == 2 ? 1 : 0);
+				glVertex3f(((vertices[tmp->points[i] - 1].x - (axis[0] + axis[1]) / 2) * cos(q) - (vertices[tmp->points[i] - 1].z - (axis[4] + axis[5]) / 2) * sin(q) + (axis[0] + axis[1]) / 2) / (axis[6] * 1.2), (vertices[tmp->points[i] - 1].y) / (axis[6] * 1.2), -(((vertices[tmp->points[i] - 1].z - (axis[4] + axis[5]) / 2) * cos(q) + (vertices[tmp->points[i] - 1].x - (axis[0] + axis[1]) / 2) * sin(q) + (axis[4] + axis[5]) / 2) / (axis[6] * 1.2)));
 			}
 		glEnd();
-		q += M_PI / (n * 200);
-		if (q >= M_PI)
-			q = -M_PI;
 		tmp = tmp->next;
+		f++;
 	}
+	q -= M_PI / 200;
+	if (q <= -M_PI)
+		q = M_PI;
 	glFlush();
 }
 
@@ -39,7 +49,6 @@ void	move_object(unsigned char key)
 {
 	if (key == 49)
 	{
-		std::cout << "left" << std::endl;
 		for (int i = 0; i < n; i++)
 			vertices[i].x -= axis[6] / 100;
 		axis[0] -= axis[6] / 100;
@@ -47,7 +56,6 @@ void	move_object(unsigned char key)
 	}
 	else if (key == 55)
 	{
-		std::cout << "right" << std::endl;
 		for (int i = 0; i < n; i++)
 			vertices[i].x += axis[6] / 100;
 		axis[0] += axis[6] / 100;
@@ -55,7 +63,6 @@ void	move_object(unsigned char key)
 	}
 	else if (key == 50)
 	{
-		std::cout << "down" << std::endl;
 		for (int i = 0; i < n; i++)
 			vertices[i].y -= axis[6] / 100;
 		axis[2] -= axis[6] / 100;
@@ -63,7 +70,6 @@ void	move_object(unsigned char key)
 	}
 	else if (key == 56)
 	{
-		std::cout << "up" << std::endl;
 		for (int i = 0; i < n; i++)
 			vertices[i].y += axis[6] / 100;
 		axis[2] += axis[6] / 100;
@@ -71,7 +77,6 @@ void	move_object(unsigned char key)
 	}
 	else if (key == 51)
 	{
-		std::cout << "backwards" << std::endl;
 		for (int i = 0; i < n; i++)
 		{
 			vertices[i].x *= 1.01;
@@ -90,7 +95,6 @@ void	move_object(unsigned char key)
 	}
 	else if (key == 57)
 	{
-		std::cout << "forwards" << std::endl;
 		for (int i = 0; i < n; i++)
 		{
 			vertices[i].x /= 1.01;
@@ -116,7 +120,13 @@ void	key_presses(unsigned char key, int x, int y)
 	if (key == 49 || key == 55 || key == 50 || key == 56 || key == 51 || key == 57)
 		move_object(key);
 	if (key == 116)
-	{} //texture apply
+	{
+		tex_apply = !tex_apply;
+		if (tex_apply)
+			glEnable(GL_TEXTURE_2D);
+		else
+			glDisable(GL_TEXTURE_2D);
+	}
 	if (key == 27)
 		exit (0);
 }
@@ -235,29 +245,100 @@ int		parse(char *filename)
 	return (0);
 }
 
+int		parse_texture(char *filename)
+{
+	std::fstream	file;
+	std::string		str;
+	std::string		tmp_data;
+	int				i = 0;
+
+	file.open(filename, std::ios::in);
+	if (!file)
+	{
+		std::cout << "Error opening file" << std::endl;
+		return (1);
+	}
+	std::getline(file, str);
+	while (file)
+	{
+		if (!str.size() || str[0] != '#')
+		{
+			if (i < 3)
+			{
+				if (i == 1)
+				{
+					int		pos = 0, pos2 = 0;
+					pos = str.find(" ", pos);
+					pos2 = str.find(" ", pos + 1);
+					width = stoi(str.substr(0, pos));
+					height = stoi(str.substr(pos + 1, pos2));
+				}
+				i++;
+				std::getline(file, str);
+				continue ;
+			}
+			else
+				tmp_data.append(str);
+		}
+		std::getline(file, str);
+	}
+	file.close();
+	std::string		tmp_data69;
+	for (int j = height - 1; j >= 0; j--)
+		for (int i = 0; i < width * 3; i += 3)
+		{
+			tmp_data69.push_back(tmp_data[i + (j * width * 3)]);
+			tmp_data69.push_back(tmp_data[i + (j * width * 3) + 1]);
+			tmp_data69.push_back(tmp_data[i + (j * width * 3) + 2]);
+		}
+	data = tmp_data69.c_str();
+	return (0);
+}
+
 int		main(int argc, char** argv)
 {
-	if (argc != 2 || !argv[1])
+	if (argc != 3 || !argv[1] || !argv[2])
 	{
-		std::cout << "Wrong number of arguments" << std::endl;
+		std::cerr << "Wrong number of arguments" << std::endl;
 		return (0);
 	}
 	if (parse(argv[1]))
 	{
-		std::cout << "Invalid file" << std::endl;
+		std::cerr << "Invalid file" << std::endl;
+		return (0);
+	}
+	if (parse_texture(argv[2]))
+	{
+		std::cerr << "Invalid file" << std::endl;
 		return (0);
 	}
 	for (int i = 0; i < 6; i++)
 		if (abs(axis[i]) > axis[6])
 			axis[6] = abs(axis[i]);
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowPosition(1600, 80);
 	glutInitWindowSize(400, 400);
 	glutCreateWindow("42");
+	glDisable(GL_TEXTURE_2D);
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+	glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0);
+	glPixelStorei(GL_UNPACK_SKIP_ROWS, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glBindTexture(GL_TEXTURE_2D, texture);
 	glutDisplayFunc(display);
 	glutKeyboardFunc(key_presses);
 	glMatrixMode(GL_PROJECTION);
+	glEnable(GL_DEPTH_TEST);
 	glLoadIdentity();
 	glutIdleFunc(idle);
 	glutMainLoop();
